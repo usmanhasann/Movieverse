7998ed5c8cdb575ac9d97f6db4827687e1b86d94
Mobeen Ahmad <Mobeen0096@gmail.com>
2024-10-11 15:06:25 +0500

    PortFolio page Update

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,6 +1,76 @@
+"use client";
 import type { NextPage } from "next";
+import { useState } from "react";
+import Image from "next/image";
+import NurtitionImg from "../../../public/imgs/portfolio/nutrition.png";
+import GrillMaster from "../../../public/imgs/portfolio/grill master.png";
+import pricebook from "../../../public/imgs/portfolio/pricebook.png";
+import FrontEndImg from "../../../public/imgs/portfolio/4.jpg";
+import pricebookCont from "../../../public/imgs/portfolio/pricebook-cont.png";
+import PricebookAdmin from "../../../public/imgs/portfolio/Pricebook-admin.png";
+import Grill2img from "../../../public/imgs/portfolio/grill-2.png";
+import SubwayImg from "../../../public/imgs/portfolio/Subway.png";
 
 const Portfolio: NextPage = () => {
+  const [activeCategory, setActiveCategory] = useState("all");
+
+  const portfolioItems = [
+    {
+      id: 1,
+      category: "web-design",
+      title: "Nutrition",
+      image: NurtitionImg,
+    },
+    {
+      id: 2,
+      category: "web-design",
+      title: "Grill Master",
+      image: GrillMaster,
+    },
+    {
+      id: 3,
+      category: "web-design",
+      title: "PriceBook HomeOwner",
+      image: pricebook,
+    },
+    {
+      id: 4,
+      category: "graphics-design",
+      title: "Front-End Development",
+      image: FrontEndImg,
+    },
+    {
+      id: 5,
+      category: "wordpress",
+      title: "PriceBook Contractors",
+      image: pricebookCont,
+    },
+    {
+      id: 6,
+      category: "web-design",
+      title: "PriceBook Admin",
+      image: PricebookAdmin,
+    },
+    {
+      id: 7,
+      category: "web-design",
+      title: "Grilli Master",
+      image: Grill2img,
+    },
+    {
+      id: 8,
+      category: "web-design",
+      title: "Subway Project",
+      image: SubwayImg,
+    },
+  ];
+
+  // Filter portfolio items based on active category
+  const filteredItems =
+    activeCategory === "all"
+      ? portfolioItems
+      : portfolioItems.filter((item) => item.category === activeCategory);
+
   return (
     <div>
       <section className="portfolio section" id="portfolio">
@@ -12,149 +82,87 @@ const Portfolio: NextPage = () => {
           </div>
           <div className="row">
             <div className="portfolio-filter padd-15">
-              <button type="button" className="active" data-filter="all">
+              <button
+                type="button"
+                className={activeCategory === "all" ? "active" : ""}
+                onClick={() => setActiveCategory("all")}
+              >
                 All
               </button>
-              <button type="button" data-filter="graphics-design">
-                Graphics Design
-              </button>
-              <button type="button" data-filter="web-design">
+              <button
+                type="button"
+                className={activeCategory === "graphics-design" ? "active" : ""}
+                onClick={() => setActiveCategory("graphics-design")}
+              >
                 Web Design
               </button>
-              <button type="button" data-filter="wordpress">
-                Wordpress
+              <button
+                type="button"
+                className={activeCategory === "web-design" ? "active" : ""}
+                onClick={() => setActiveCategory("web-design")}
+              >
+                ReacT js
+              </button>
+              <button
+                type="button"
+                className={activeCategory === "wordpress" ? "active" : ""}
+                onClick={() => setActiveCategory("wordpress")}
+              >
+                Next Js
               </button>
             </div>
           </div>
+
           <div className="row">
-            {/* Portfolio Item */}
-            <div className="portfolio-item padd-15" data-category="web-design">
-              <div className="portfolio-item-inner shadow-dark">
-                <div className="portfolio-img">
-                  <img src="/imgs/portfolio/nutrition.png" alt="portfolio" />
-                </div>
-                <div className="portfolio-info">
-                  <h4>Nutrition</h4>
-                  <div className="icon">
-                    <i className="fa fa-search" />
+            {filteredItems.length > 0 ? (
+              filteredItems.map((item) => (
+                <div
+                  key={item.id}
+                  className="portfolio-item padd-15"
+                  data-category={item.category}
+                >
+                  <div className="portfolio-item-inner shadow-dark">
+                    <div className="portfolio-img">
+                      <Image src={item.image} alt={item.title} />
+                    </div>
+                    <div className="portfolio-info">
+                      <h4>{item.title}</h4>
+                      <div className="icon">
+                        <i className="fa fa-search" />
+                      </div>
+                    </div>
                   </div>
                 </div>
+              ))
+            ) : (
+              <div className="coming-soon padd-15">
+                <h3>Coming Soon</h3>
+                <p>We are working on adding more content for this category.</p>
               </div>
-            </div>
-            {/* Portfolio Item End*/}
-            {/* Portfolio Item */}
-            <div className="portfolio-item padd-15" data-category="web-design">
-              <div className="portfolio-item-inner shadow-dark">
-                <div className="portfolio-img">
-                  <img src="/imgs/portfolio/grill master.png" alt="portfolio" />
-                </div>
-                <div className="portfolio-info">
-                  <h4>Grill Master</h4>
-                  <div className="icon">
-                    <i className="fa fa-search" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Portfolio Item End*/}
-            {/* Portfolio Item */}
-            <div className="portfolio-item padd-15" data-category="web-design">
-              <div className="portfolio-item-inner shadow-dark">
-                <div className="portfolio-img">
-                  <img src="/imgs/portfolio/pricebook.png" alt="portfolio" />
-                </div>
-                <div className="portfolio-info">
-                  <h4>PriceBook HomeOwner</h4>
-                  <div className="icon">
-                    <i className="fa fa-search" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Portfolio Item End*/}
-            {/* Portfolio Item */}
-            <div
-              className="portfolio-item padd-15"
-              data-category="graphics-design"
-            >
-              <div className="portfolio-item-inner shadow-dark">
-                <div className="portfolio-img">
-                  <img src="/imgs/portfolio/4.jpg" alt="portfolio" />
-                </div>
-                <div className="portfolio-info">
-                  <h4>Front-End Development</h4>
-                  <div className="icon">
-                    <i className="fa fa-search" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Portfolio Item End*/}
-            {/* Portfolio Item */}
-            <div className="portfolio-item padd-15" data-category="wordpress">
-              <div className="portfolio-item-inner shadow-dark">
-                <div className="portfolio-img">
-                  <img
-                    src="/imgs/portfolio/pricebook-cont.png"
-                    alt="portfolio"
-                  />
-                </div>
-                <div className="portfolio-info">
-                  <h4>PriceBook Contractors</h4>
-                  <div className="icon">
-                    <i className="fa fa-search" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Portfolio Item End*/}
-            {/* Portfolio Item */}
-            <div className="portfolio-item padd-15" data-category="web-design">
-              <div className="portfolio-item-inner shadow-dark">
-                <div className="portfolio-img">
-                  <img
-                    src="/imgs/portfolio/Pricebook-admin.png"
-                    alt="portfolio"
-                  />
-                </div>
-                <div className="portfolio-info">
-                  <h4>PriceBook Admin</h4>
-                  <div className="icon">
-                    <i className="fa fa-search" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="portfolio-item padd-15" data-category="web-design">
-              <div className="portfolio-item-inner shadow-dark">
-                <div className="portfolio-img">
-                  <img src="/imgs/portfolio/grill-2.png" alt="portfolio" />
-                </div>
-                <div className="portfolio-info">
-                  <h4>Grilli Master</h4>
-                  <div className="icon">
-                    <i className="fa fa-search" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="portfolio-item padd-15" data-category="web-design">
-              <div className="portfolio-item-inner shadow-dark">
-                <div className="portfolio-img">
-                  <img src="/imgs/portfolio/Subway.png" alt="portfolio" />
-                </div>
-                <div className="portfolio-info">
-                  <h4>Subway Project</h4>
-                  <div className="icon">
-                    <i className="fa fa-search" />
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Portfolio Item End*/}
+            )}
           </div>
         </div>
       </section>
+
+      <style jsx>{`
+        .coming-soon {
+          text-align: center;
+          color: #ff6347; /* or any other color you'd like */
+          font-style: italic;
+          padding: 50px 0;
+          background-color: #f0f0f0;
+          border-radius: 10px;
+        }
+
+        .coming-soon h3 {
+          font-size: 2rem;
+          margin-bottom: 10px;
+        }
+
+        .coming-soon p {
+          font-size: 1.2rem;
+        }
+      `}</style>
     </div>
   );
 };
